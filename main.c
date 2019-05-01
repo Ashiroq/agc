aadffd32b430e941a3f6f1412b83867973d6b5aa
ashiroq <30812031+Ashiroq@users.noreply.github.com>
2019-05-01 14:40:33 +0200

    Working object storing

--- main.c
+++ main.c
@@ -6,10 +6,77 @@
 #include <zlib.h>
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define CHUNK 16384
 
-int compressFile(FILE* source, FILE* dest, int level)
+// TODO: Trees
+
+/* Decompress from file source to file dest until stream ends or EOF.
+   inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+   allocated for processing, Z_DATA_ERROR if the deflate data is
+   invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
+   the version of the library linked do not match, or Z_ERRNO if there
+   is an error reading or writing the files. */
+int inf(FILE *source, FILE *dest)
+{
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    ret = inflateInit(&strm);
+    if (ret != Z_OK)
+        return ret;
+
+    /* decompress until deflate stream ends or end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
+
+        /* run inflate() on input until output buffer not full */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = inflate(&strm, Z_NO_FLUSH);
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            switch (ret) {
+            case Z_NEED_DICT:
+                ret = Z_DATA_ERROR;     /* and fall through */
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                (void)inflateEnd(&strm);
+                return ret;
+            }
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
+
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+}
+
+int def_with_header(FILE* source, FILE* dest, int level, const char* header, int hsize)
 {
     int ret, flush;
     unsigned int amount;
@@ -24,10 +91,30 @@ int compressFile(FILE* source, FILE* dest, int level)
     if(ret != Z_OK)
         return ret;
 
+    /* deflating header */
+    char* hcopy = malloc(hsize * sizeof(hcopy[0]));
+    strcpy(hcopy, header);
+    stream.next_in = hcopy;
+    stream.avail_in = hsize;
+    do {
+        stream.avail_out = CHUNK;
+        stream.next_out = out;
+        ret = deflate(&stream, flush);
+        assert(ret != Z_STREAM_ERROR);
+        amount = CHUNK - stream.avail_out;
+        if(fwrite(out, 1, amount, dest) != amount || ferror(dest)) {
+            deflateEnd(&stream);
+            return Z_ERRNO;
+        }
+    } while(stream.avail_out == 0);
+    assert(stream.avail_in == 0);
+    free(hcopy);
+
+    /* deflating data until eof */
     do {
         stream.avail_in = fread(in, 1, CHUNK, source);
         if(ferror(source)) {
-            (void)deflateEnd(&stream);
+            deflateEnd(&stream);
             return Z_ERRNO;
         }
         flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
@@ -39,7 +126,7 @@ int compressFile(FILE* source, FILE* dest, int level)
             assert(ret != Z_STREAM_ERROR);
             amount = CHUNK - stream.avail_out;
             if(fwrite(out, 1, amount, dest) != amount || ferror(dest)) {
-                (void)deflateEnd(&stream);
+                deflateEnd(&stream);
                 return Z_ERRNO;
             }
         } while(stream.avail_out == 0);
@@ -47,25 +134,57 @@ int compressFile(FILE* source, FILE* dest, int level)
     } while(flush != Z_FINISH);
     assert(ret == Z_STREAM_END);
 
-    (void)deflateEnd(&stream);
+    deflateEnd(&stream);
     return Z_OK;
 }
 
 int addFile(const char* name)
 {
-    unsigned char hash[40];
-    SHA1(name, strlen(name), hash);
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    unsigned char buffer[CHUNK];
+    SHA_CTX ctx;
+    int amount;
 
-    char tmp = hash[3];
-    hash[3] = '\0';
+    FILE* src = fopen(name, "rb");
+    if(src == NULL)
+        return 19;
+    SHA1_Init(&ctx);
     struct stat st = {0};
-    if(stat(hash, &st) == -1) {
-        mkdir(hash, 0700);
-    }
-    hash[3] = tmp;
-    FILE* obj = fopen(hash+2, "w");
-    FILE* src = fopen(name, "r");
-    int ret = compressFile(src, obj, 6);
+    assert(stat(name, &st) == 0);
+
+    /* calculating hash with added header */
+    char header[100];
+    int hsize = sprintf(header, "blob %d", st.st_size);
+    SHA1_Update(&ctx, header, hsize + 1);               /* +1 because \0 is needed */
+    while((amount = fread(buffer, 1, CHUNK, src)) != 0)
+        SHA1_Update(&ctx, buffer, amount);
+    SHA1_Final(hash, &ctx);
+
+    char* hexhash = malloc((2 * SHA_DIGEST_LENGTH + 1) * sizeof(hexhash[0]));
+    for(int i = 0; i < SHA_DIGEST_LENGTH; i++)
+        sprintf(hexhash + 2*i, "%02x", hash[i]);
+
+    /* building hash-based path for file */
+    char path[13 + 42] = ".agc/objects/";
+    memcpy(path+13, hexhash, 2);            /* append directory name */
+    path[15] = '/';
+    struct stat st2 = {0};
+    if(stat(path, &st2) == -1)
+        mkdir(path, 0700);
+    memcpy(path+16, hexhash+2, 38);         /* append file name */
+    path[54] = '\0';
+
+    printf("%s\n", hexhash);
+    free(hexhash);
+
+    FILE* dest = fopen(path, "wb");
+    /* go back to beginning of file after hashing */
+    rewind(src);
+    /* file compression with header  */
+    int ret = def_with_header(src, dest, Z_DEFAULT_COMPRESSION, header, hsize + 1);
+
+    fclose(src);
+    fclose(dest);
     return ret;
 }
 
@@ -96,10 +215,13 @@ int main(int argc, char** argv)
             return 1;
         }
         int err = addFile(argv[2]);
-        if(err != 0) {
-            printf("error %d: cannot add file", err);
-            return err;
-        }
+        return err;
+    }
+    else if(strcmp(argv[1], "cat") == 0) {
+        FILE* src = fopen(argv[2], "rb");
+        FILE* dest = fopen("testdest", "wb");
+        int ret = inf(src, dest);
+        return ret;
     }
     else {
         printf("Invalid command");
